d8c82eb2d3bdc18a1cf1ae19c0b3431108f31b3c
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2021-03-29 18:58:06 +0530

    Set new values of variables that are initialized using Constructors

---
+++ OOPS/Constructors/XAndYValues.java
@@ -0,0 +1,33 @@
+// Program to set new values of variables that are initialized using Constructor
+
+import java.util.Scanner;
+
+class XAndYValues{
+    private int x;
+    private int y;
+    
+    public XAndYValues(int x, int y){
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+    
+    void changeXAndY(int newx, int newy){
+        this.x = newx;
+        this.y = newy;
+    }
+    
+    void printXAndY(){
+        System.out.println("New x = " + x);
+        System.out.println("New y = " + y);
+    }
+    public static void main(String[] args){
+        XAndYValues m = new XAndYValues(5,10);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter new values of x and y: ");
+        m.x = sc.nextInt();
+        m.y = sc.nextInt();
+        m.changeXAndY(m.x,m.y);
+        System.out.println("New x and y are : ");
+        m.printXAndY();
+    }
+}
